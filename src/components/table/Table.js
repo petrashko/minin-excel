8b624e757c715e9cded670ada42b1d3b08b58482
petrashko <petrashko@tpu.ru>
2022-11-17 20:23:50 +0700

    Add function calc to CSS for min-height of table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -58,15 +58,8 @@ class Table extends ExcelComponent {
     init() {
         super.init();
 
-        // const documentHeight = Math.max(
-        //     document.body.scrollHeight, document.documentElement.scrollHeight,
-        //     document.body.offsetHeight, document.documentElement.offsetHeight,
-        //     document.body.clientHeight, document.documentElement.clientHeight
-        // );
-
-        const tableMaxHeight = document.documentElement.clientHeight - 98;
-        //console.log(this.$root);
-        this.$root.$el.style.maxHeight = `${tableMaxHeight}px`;
+        //const tableMaxHeight = document.documentElement.clientHeight - 98;
+        //this.$root.$el.style.maxHeight = `${tableMaxHeight}px`;
     }
 }
 
